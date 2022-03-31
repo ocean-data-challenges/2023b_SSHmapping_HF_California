42e0a399f10a467f508f808168ef48b554058baa
Sammy Metref <metrefsammy@gmail.com>
2022-03-31 13:32:41 +0200

    Adding mod_prep_colab.py 2

--- src/mod_prep_colab.py
+++ src/mod_prep_colab.py
@@ -2,9 +2,9 @@ import os
 os.system('pip install -q condacolab')
 import condacolab
 condacolab.install()
-
-os.system('conda install -c conda-forge/label/cf202003 pyinterp')
-os.system('conda install -c conda-forge hvplot')
-os.system('conda install -c conda-forge/label/cf202003 pyinterp')
-os.system('conda install -c conda-forge xrft')
-os.system('conda install -c conda-forge gcsfs')
+os.system('!conda install -y -c ipykernel')
+os.system('conda install -y -c conda-forge/label/cf202003 pyinterp')
+os.system('conda install -y -c conda-forge hvplot')
+os.system('conda install -y -c conda-forge/label/cf202003 pyinterp')
+os.system('conda install -y -c conda-forge xrft')
+os.system('conda install -y -c conda-forge gcsfs')
